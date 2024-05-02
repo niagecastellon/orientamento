5847afdf362aaff0301f9a1f140261828bf6f72f
giacomo-fantin <116014755+giacomo-fantin@users.noreply.github.com>
2024-05-02 13:23:01 +0000

    creato branch per angular

---
+++ app-orientamento/src/app/scuola/scuola.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scuola',
+  standalone: true,
+  imports: [],
+  templateUrl: './scuola.component.html',
+  styleUrl: './scuola.component.css'
+})
+export class ScuolaComponent {
+
+}
